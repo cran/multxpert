bca3a82b253bb538057099fb5b1e235a2d986841
Eric Nantz <eric.nantz@gmail.com>
2019-10-02 14:54:00 +0000

    version 0.1.1

--- R/zzz.R
+++ R/zzz.R
@@ -4,10 +4,10 @@
 # For more information about the Multxpert package, visit the Multiplicity Expert web site
 # http://multxpert.com/wiki/MultXpert_package
 
-.onLoad <- function(lib, pkg) {
+.onAttach <- function(lib, pkg) {
 	if (interactive()) {
-		cat('multxpert: Implementation of commonly used p-value based and parametric\n',
-				'multiple testing procedures and parallel gatekeeping procedures.\n',
-				'For more information visit http://multxpert.com/wiki/MultXpert_package\n', sep='')
+            packageStartupMessage('multxpert: Implementation of commonly used p-value based and parametric\n',
+                                  'multiple testing procedures and parallel gatekeeping procedures.\n',
+                                  'For more information visit http://multxpert.com/wiki/MultXpert_package\n')
 	}
 }
